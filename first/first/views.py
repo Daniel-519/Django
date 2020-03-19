1507dd0fdcd6637f870ee5f464f41313b9a56940
LaremyWade <laremy.wade@outlook.com>
2020-03-18 18:35:44 -0600

    Simple Polls App and Packaging

---
+++ first/first/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from polls.models import Question
+
+
+def home(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'index.html', context)
--- wisdompets/wisdompets/urls.py
+++ wisdompets/wisdompets/urls.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from django.conf.urls import url
 
 from adoptions import views
+from django.urls import path, include
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
     url(r'^pets/(\d+)/', views.pet_detail, name='pet_detail'),
+    path('polls/', include('polls.urls')),
 ]
 
 # {% url %}
